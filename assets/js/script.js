9ff800a6cec8d973bcfdb62e1d5e904147830ab5
allielynne20 <allielynne20@gmail.com>
2020-07-20 22:02:11 -0400

    formatted the phone numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,20 @@ $("#searchBtn").on("click", function () {
     cityName.value = "";
 })
 
-
+// Fetch BrewApi information
 function saveCityBrew(cityName) {
 
     fetch(
         'https://api.openbrewerydb.org/breweries?by_city=' + cityName)
         .then(res => res.json())
         .then(function (data) {
+            //locally store data for future email form
+            localStorage.setItem('brewresults', data.length);
             Brew(data);
         });
 }
 
-
+// Format brew info and add to page
 function Brew(data) {
     console.log(data)
     callout1.innerHTML = "";
@@ -38,8 +40,12 @@ function Brew(data) {
             var brewSite = $('<a>').addClass("foodsite").attr('href', data[i].website_url).text(data[i].website_url);
             //alter to make it a hyperlink
             var brewPhone = $('<p>').addClass("foodphone tel").text(data[i].phone);
+            //this formats the phone number into 000-000-0000
+            $(brewPhone).text(function(i, text) {
+                text = text.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+                return text;
+            });
             //alter to make it in correct format
-
             brewEl.append(brewName);
             brewEl.append(brewStreet);
             brewEl.append(brewPhone);
@@ -59,6 +65,8 @@ function saveCityEvent(cityName) {
         })
         .then(function (data) {
             console.log(data);
+            //locally store data for future email form 
+            localStorage.setItem('eventresults', data.length);
             eventsToPage(data);
         });
 };
@@ -73,12 +81,10 @@ function eventsToPage(data) {
             var eventList = $('<p>').addClass("eventlist").text(data._embedded.events[i].name);
             var eventDate = $('<p>').addClass("eventdate").text('Event Date: ' + data._embedded.events[i].dates.start.localDate);
             var ticketImg = $('<img>').addClass('ticketicon').attr('src', './assets/images/ticketicon.png');
-
             //add ticket info buttons
             ticketButtonId = ("ticketButton" + [i]);
             var ticketInfo = $('<button>').addClass("eventbutton").attr("onclick", "window.location.href='" + (data._embedded.events[i].url) + "';");
             ticketInfo.append(ticketImg)
-
             // Append elements to eventEl
             eventEl.append(eventList);
             eventEl.append(eventDate);
@@ -87,3 +93,4 @@ function eventsToPage(data) {
         };
     };
 };
+
